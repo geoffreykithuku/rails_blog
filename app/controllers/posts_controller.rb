e44c92e5ff15a9f759fb663169a83536157a62ff
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-01-23 23:11:29 +0300

    implement user authentication using devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+
 before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+
 
   def new
     @post = Post.new
