30688144a78ab5ed215a51a12973dd86001ee8e8
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-01-23 14:29:38 +0300

    fix form errors by disabling turbo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,22 @@ class PostsController < ApplicationController
 
   end
 
-  def create
-  end
+ def create
+  @post = Post.new(post_params)
 
+  if @post.save
+    redirect_to @post
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
